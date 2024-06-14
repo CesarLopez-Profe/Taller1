5cda79cd9843b884b4b5bd94b63e9d14c07414ee
CesarLopez-Profe <112127464+CesarLopez-Profe@users.noreply.github.com>
2024-06-14 18:03:58 -0500

    Add files via upload

---
+++ src/main/java/taller1/Calculos.java
@@ -0,0 +1,13 @@
+package taller1;
+
+public class Calculos {
+
+    public int ValAbs(int nro){
+        int val_ret = nro;
+        if (nro < 0) 
+            val_ret = nro * -1;
+
+        return val_ret;
+    }
+
+}
---
+++ src/main/java/taller1/Main.java
@@ -0,0 +1,8 @@
+package taller1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("El valor absoluto es: \n" + new Calculos().ValAbs(-5));
+    }
+
+}
